8e0c9a0e19e351fbae867ceba983511b07905657
KAMRAN <Kami.zare74@gmail.com>
2025-03-24 19:16:40 +0330

    use sqlfile for dapper doesnt Complate

---
+++ Application/Commands&Queries/Users/Commands/SQL/InsertRandomUser.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO Users 
+(Name, Email, Password, UserName, NationalCode, CreateDate, EditationDate, UserCreate, UserEditation)
+VALUES 
+(@Name, @Email, @Password, @UserName, @NationalCode, @CreateDate, @EditationDate, @UserCreate, @UserEditation)
